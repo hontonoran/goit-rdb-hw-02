86bd60b03acf4d3400f4b1e29180cc6bba2a80ba
hontonoran <hontonoran@gmail.com>
2026-09-26 19:12:07 +0300

    add products table and ordernumber field, update screenshots and readme

--- insert_values.sql
+++ insert_values.sql
@@ -1,17 +1,22 @@
 USE hw02;
-
+ 
 INSERT INTO clients (client_name, client_address) VALUES
 ('Мельник', 'Хрещатик 1'),
 ('Шевченко', 'Басейна 2'),
 ('Коваленко', 'Комп''ютерна 3');
-
-INSERT INTO orders (order_id, client_id, order_date) VALUES
-(101, 1, '2023-03-15'),
-(102, 2, '2023-03-16'),
-(103, 3, '2023-03-17');
-
-INSERT INTO order_details (order_id, product_name, quantity) VALUES
-(101, 'Лептоп', 3),
-(101, 'Мишка', 2),
-(102, 'Принтер', 1),
-(103, 'Мишка', 4);
+ 
+INSERT INTO products (product_name) VALUES
+('Лептоп'),
+('Мишка'),
+('Принтер');
+ 
+INSERT INTO orders (order_number, client_id, order_date) VALUES
+('101', 1, '2023-03-15'),
+('102', 2, '2023-03-16'),
+('103', 3, '2023-03-17');
+ 
+INSERT INTO order_details (order_id, product_id, quantity) VALUES
+(1, 1, 3),
+(1, 2, 2),
+(2, 3, 1),
+(3, 2, 4);
--- сreate_tables.sql
+++ сreate_tables.sql
@@ -1,25 +1,32 @@
-CREATE DATABASE IF NOT EXISTS hw02;
+CREATE DATABASE hw02;
 USE hw02;
-
+ 
 CREATE TABLE clients (
     client_id INT AUTO_INCREMENT PRIMARY KEY,
     client_name VARCHAR(100) NOT NULL,
     client_address VARCHAR(255) NOT NULL
 );
-
+ 
+CREATE TABLE products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL UNIQUE
+);
+ 
 CREATE TABLE orders (
-    order_id INT PRIMARY KEY,
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_number VARCHAR(20) NOT NULL UNIQUE,
     client_id INT NOT NULL,
     order_date DATE NOT NULL,
     FOREIGN KEY (client_id) REFERENCES clients(client_id)
 );
-
+ 
 CREATE TABLE order_details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
-    product_name VARCHAR(100) NOT NULL,
+    product_id INT NOT NULL,
     quantity INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    UNIQUE (order_id, product_name),
+    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    UNIQUE (order_id, product_id),
     CHECK (quantity > 0)
 );
